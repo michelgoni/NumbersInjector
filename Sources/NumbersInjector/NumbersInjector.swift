3539e074ae4dda331ca4a9700df178cf6dc844c3
Michel Goñi <mgoni@wabiproject.com>
2023-01-24 09:46:54 +0100

    Added simple implememtation for the NumbersInjector

--- Sources/NumbersInjector/NumbersInjector.swift
+++ Sources/NumbersInjector/NumbersInjector.swift
@@ -2,5 +2,22 @@ import Foundation
 import Swinject
 
 public final class NumbersInjector {
-    
+
+    public static let shared = NumbersInjector()
+    private lazy var assembler = Assembler()
+    private lazy var container = assembler.resolver as? Container
+    private lazy var resolver = container?.synchronize()
+
+    private init() {}
+
+    func apply(_ assemblies: [Assembly]) {
+        assembler.apply(assemblies: assemblies)
+    }
+
+}
+
+public extension NumbersInjector {
+    func get<Service>(_ service: Service.Type) -> Service? {
+        resolver?.resolve(service)
+    }
 }
